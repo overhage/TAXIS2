4aa024afb7148e719aa1250e2e1ef3e8b7846c19
J. Marc Overhage <overhage@gmail.com>
2025-09-07 16:15:10 -0400

    tailwind config fix

--- tailwind/config.js
+++ tailwind/config.js
@@ -1,9 +1,9 @@
 // tailwind.config.js
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: { extend: {} },
-  plugins: []
+  plugins: [],
 }
